4c1fcc74655a07593ad80549d52d5bfc42b23ef3
tomasz <csharp@406-00>
2026-09-11 14:21:16 +0200

    q

--- P03WeatherForecastWPF.Client/Services/ProductService.cs
+++ P03WeatherForecastWPF.Client/Services/ProductService.cs
@@ -4,6 +4,7 @@ using P06Shop.Shared.Services.ProductService;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 
 namespace P03WeatherForecastWPF.Client.Services
@@ -17,11 +18,39 @@ namespace P03WeatherForecastWPF.Client.Services
             _httpClient = httpClient;
         }
 
+        public async Task<ServiceResponse<Product>> CreateProductAsync(Product product)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/product", product);
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Product>>();
+            return result;
+        }
+
+        public async Task<ServiceResponse<bool>> DeleteProductAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/product/{id}");
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+            return result;
+        }
+
+        public Task<ServiceResponse<Product>> GetProductAsync(int id)
+        {
+             var response = _httpClient.GetAsync($"api/product/{id}");
+             var result = response.Result.Content.ReadFromJsonAsync<ServiceResponse<Product>>();
+             return result;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
         {
             var response = await _httpClient.GetAsync("api/product");
             var json = await response.Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<ServiceResponse<List<Product>>>(json);
+            return result;
+        }
+
+        public async Task<ServiceResponse<Product>> UpdateProductAsync(Product product)
+        {
+            var response = await _httpClient.PutAsJsonAsync("api/product", product);
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Product>>();
             return result;
         }
     }
