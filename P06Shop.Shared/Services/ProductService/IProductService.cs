967e1b0b585a4c2d7b659ee041bf507eaf203cd8
tomasz <csharp@406-00>
2026-09-11 11:20:14 +0200

    q

--- P06Shop.Shared/Services/ProductService/IProductService.cs
+++ P06Shop.Shared/Services/ProductService/IProductService.cs
@@ -7,5 +7,13 @@ namespace P06Shop.Shared.Services.ProductService
     public interface IProductService
     {
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
+
+        Task<ServiceResponse<Product>> GetProductAsync(int id);
+
+        Task<ServiceResponse<Product>> CreateProductAsync(Product product);
+
+        Task<ServiceResponse<bool>> DeleteProductAsync(int id);
+
+        Task<ServiceResponse<Product>> UpdateProductAsync(Product product);
     }
 }
